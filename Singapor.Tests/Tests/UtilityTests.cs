bdd0bb0a31d0d4565708625b4a1ef816ec43e29c
pavel shtanko <p.shtanko@synergysportstech.com>
2016-04-14 18:47:28 +0300

    remove commented chunk of code

--- Singapor.Tests/Tests/UtilityTests.cs
+++ Singapor.Tests/Tests/UtilityTests.cs
@@ -9,11 +9,6 @@ namespace Singapor.Tests.Tests
     [TestClass]
     public class UtilityTests : UnitTestBase
     {
-        //public Guid? Id { get; set; }
-        //public string Name { get; set; }
-        //public string Description { get; set; }
-        //public Guid? CompanyId
-
         #region Public methods
 
         [TestMethod]
